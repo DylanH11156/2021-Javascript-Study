fa04dbca1cd1f3174b78bc504761adfa5ab6aa4c
DylanH11156 <dylan.hammer@vtsd.com>
2022-01-21 07:57:15 -0500

    Java Application Notes

---
+++ mini challenge/challenge.js
@@ -0,0 +1,9 @@
+function showButton(){
+    var btn= document.getElementById("btn")
+    btn.style.display="block";
+    font-size: 75px;
+}
+function bigImg(){
+    var img = document.getElementById("image");
+    img.style.width= "100%";
+}
